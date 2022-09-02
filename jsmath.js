dbf474d6948479d525c04e67a7af26d2060834f3
RAVVA2799 <80970669+RAVVA2799@users.noreply.github.com>
2022-09-02 11:49:01 +0530

    Add files via upload

---
+++ jsmath.js
@@ -0,0 +1,30 @@
+//JS MATH FUNCTIONS //
+
+//MATH TRUNCATE FUNCTION RETURNS ONLY THE INTEGER PART AND IGNORES THE DECIMAL PART.
+
+const k=Math.trunc(4.7);
+console.log(k);
+console.log(Math.trunc(-8.67));
+
+//.............................//
+//MATH SIGN WILL RETURN -1,0,1 IF THE NUMBERS ARE NEGATIVE, ZERO OR POSITIVE//
+
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+console.log(Math.sign(+56));
+
+//...............................//
+
+//math cbrt will return the cube root of the given number.
+
+console.log(Math.cbrt(125));
+console.log(Math.cbrt(-64));
+//math sqrt will return the square root of the numbers.//
+console.log(Math.sqrt(125));
+console.log(Math.sqrt(64));
+
+//math log base 10 will give u log10(x) value//
+ console.log(Math.log10(300)); 
+
+ //math log base 10 will give u log2(x) value//
+ console.log(Math.log2(8)); 
